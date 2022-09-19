4438e5e114b8633fb76f5402d13fcd46b51aff0f
isaacmasinde <isaacmasinde2019@gmail.com>
2022-09-19 11:10:31 +0300

    modified:   public/script.js 	modified:   views/room.ejs

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 const socket = io('/')
+let displayMediaStream
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
     host: '/',
@@ -10,26 +11,84 @@ const myPeer = new Peer(undefined, {
 
 const myVideo = document.createElement('video')
 myVideo.muted=true
-
+let calls = [];
 const peers ={}
 navigator.mediaDevices.getUserMedia({
     video: true,
-    audio:true
+    audio:false
 }).then( stream => {
     addVideoStream(myVideo, stream)
 
     myPeer.on('call', call  => {
         call.answer(stream)
         video = document.createElement('video')
-        
         call.on('stream', UserVideoStream => {
             addVideoStream(video, UserVideoStream)
         })
+        console.log(call.peer)
+        peers[call.peer] = call
+        calls.push(call)
     })
     socket.on('user-connected', userId => {
-        console.log('New user connected 5')
         connectToNewUser(userId, stream)
+        console.log(peers)
     })
+
+    document.getElementById('sharescreen').addEventListener('click', async () => {
+        if (!displayMediaStream) {
+          displayMediaStream = await navigator.mediaDevices.getDisplayMedia();
+        }
+        console.log(calls);
+        calls.forEach((call) => {
+            console.log("got here")
+            const sender = call.peerConnection.getSenders().find((s) => s.track.kind === 'video');
+            console.log('Found sender:', sender);
+            sender.replaceTrack(displayMediaStream.getVideoTracks()[0]);
+        });
+
+        
+        //show what you are showing in your "self-view" video.
+        myVideo.srcObject = displayMediaStream;
+        
+
+      });
+      stream.getVideoTracks()[0].onended = function () {
+        console.log(calls);
+        calls.forEach((call) => {
+            console.log("got here")
+            const sender = call.peerConnection.getSenders().find((s) => s.track.kind === 'video');
+            console.log('Found sender:', sender);
+            sender.replaceTrack(stream.getVideoTracks()[0]);
+        });
+
+        
+        //show what you are showing in your "self-view" video.
+        myVideo.srcObject = stream;
+      };
+    document.getElementById("mutecam").addEventListener("click", 
+    function () {
+        const videoTrack = stream.getTracks().find(track => track.kind == 'video');
+        if (videoTrack.enabled){
+            videoTrack.enabled = false;
+        }
+        else{
+            videoTrack.enabled = true;
+        }
+    }
+    );
+
+    document.getElementById("mutemic").addEventListener("click", 
+    function () {
+        const audioTrack = stream.getTracks().find(track => track.kind == 'audio');
+        if (audioTrack.enabled){
+            audioTrack.enabled = false;
+        }
+        else{
+            audioTrack.enabled = true;
+        }
+    }
+    );
+    
 })
 
 socket.on('user-disconnected', userId => {
@@ -50,6 +109,7 @@ function connectToNewUser(userId, stream){
     })
 
     peers[userId] = call
+    calls.push(call)
 }
 function addVideoStream(video, stream){
     video.srcObject = stream
